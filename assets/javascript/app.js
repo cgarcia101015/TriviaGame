aeea97b6f4f2df3bac80cfb200318badfcfef625
Carlos M Garcia <cgarcia101015@gmail.com>
2018-10-24 01:20:23 -0400

    working on countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,8 @@
-function startTimer(duration, display) {
-    var timer = duration,  seconds;
-    setInterval(function () {
-        
-        seconds = parseInt(timer % 60, 10);
+var myVar = setInterval(myTimer, 1000);
 
-        
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text( seconds);
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-jQuery(function ($) {
-    var fiveMinutes = 60 * 5,
-        display = $('#time');
-    startTimer(fiveMinutes, display);
-});
+function myTimer() {
+    var timeLeft = 60;
+    for(timeLeft = 60; timeLeft > 0; timeLeft--)
+    
+    console.log(timeLeft);
+};
