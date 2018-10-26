d8a1a1a693d7e47c6eda6a1b9f8bdd5f5b9f5aa2
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2018-10-25 21:57:22 -0400

    fixed timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,16 @@
-(function count(cc) {
-    document.getElementById("cco").innerHTML = cc;
-    if (cc > 0)
-        setTimeout(function() { count(--cc); }, 1000);
-  })(60);
+// (function count(cc) {
+//     document.getElementById("cco").innerHTML = cc;
+//     if (cc > 0)
+//         setTimeout(function() { count(--cc); }, 1000);
+//   })(60);
+
+  var count = 60;
+  var timer = setInterval(function() {
+      $("#cco").html(count)
+      count--;
+      if(count === 0) {
+          clearInterval (timer);
+          $("#cco").html("00");
+      }
+      
+  },1000 );
