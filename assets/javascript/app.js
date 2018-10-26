24043bd17d70433a348cbbb0502146044538ec59
Carlos M Garcia <cgarcia101015@gmail.com>
2018-10-25 23:30:00 -0400

    one answer up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,29 @@
-// (function count(cc) {
-//     document.getElementById("cco").innerHTML = cc;
-//     if (cc > 0)
-//         setTimeout(function() { count(--cc); }, 1000);
-//   })(60);
 
-  var count = 60;
-  var timer = setInterval(function() {
-      $("#cco").html(count)
-      count--;
-      if(count === 0) {
-          clearInterval (timer);
-          $("#cco").html("00");
-      }
-      
-  },1000 );
+
+var count = 60;
+var timer = setInterval(function () {
+    $("#cco").html(count)
+    count--;
+    if (count === 0) {
+        clearInterval(timer);
+        $("#cco").html("00");
+    }
+
+}, 1000);
+
+
+var q1 = ["What is the name of Will Smith’s character in Independence Day?", "Captain Steven Hiller", "Captain Ron", "Captain America"];
+
+$("#questions").append(q1[0]);
+$("#choices").append(q1[1]);
+// $("#choices").append(q1[2] + "     ");
+// $("#choices").append(q1[3] + "     ");
+
+var answer1 = [];
+answer1 = q1;
+
+for (i = 0; i <= 3; i++) {
+    answer1.push($('<div>', {i} ));
+};
+$("#choices").append();
+
