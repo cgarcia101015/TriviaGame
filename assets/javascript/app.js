c369fb1eb039f07b8350552e0c8414f81f674399
Carlos M Garcia <cgarcia101015@gmail.com>
2018-10-27 01:12:52 -0400

    added all questions and buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
 
-
-
 //Countdown Timer
 function timer() {
 var count = 60;
@@ -15,51 +13,82 @@ var timer = setInterval(function () {
     }, 1000);
 };
 
-
+//Game score
+var score = 0;
 
 //Questions and answers Object
 var questions = [
     {
         q0: "What is the name of Will Smith’s character in Independence Day?",
-        answers1: ["Captain Steven Hiller", "Captain Ron", "Captain America"],
+        answers0: ["Captain Steven Hiller", "Captain Ron", "Captain America"],
         correctAnswer1: 0,
     },
 
     {
         q1: "Rap was just becoming well known in the 90s. Which artist had the first number one rap single?",
-        answers2: ["Marky Mark", "MC Hammer", "Vanills Ice"],
-        correctAnswer2: 2,
+        answers1: ["Marky Mark", "MC Hammer", "Vanills Ice"],
+        correctAnswer1: 2,
     },
 
     {
         q2: "This fad of the '90s was like a purse carried on your waist.",
-        answers3: ["Ruck Sack", "Fanny Pack", "Pocket Book"],
-        correctAnswer3: 1,
+        answers2: ["Ruck Sack", "Fanny Pack", "Pocket Book"],
+        correctAnswer2: 1,
     },
 
     {
         q3: "Which musical star was knighted by Queen Elizabeth II on February 24 1998?",
-        answers4: ["Elton John", "Bono", "George Michael"],
-        correctAnswer4: 0,
+        answers3: ["Elton John", "Bono", "George Michael"],
+        correctAnswer3: 0,
     },
 
     {
         q4: "The original run of which 'Star Trek' series ended in 1999?",
-        answers5: ["The Next Generation", "Deep Space Nine", "Voyager"],
-        correctAnswer5: 1,
+        answers4: ["The Next Generation", "Deep Space Nine", "Voyager"],
+        correctAnswer4: 1,
     },
 ];
 
-function loadTrivia() {
-    for (i = 0; i <=4; i++) {
+// function loadTrivia() {
+//     for (i = 0; i <=5; i++) {
 
-        $('#questions').append('<input type="radio" name="radio_name" />' + questions[0]);
-        }
-    };
+    //Loading questions to page
+        $('#questions').append(questions[0].q0 + '<br>');
+        $('#questions').append('<input type="radio" name="radio_name" id="q00" />' + questions[0].answers0[0]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" "id="q01" />' + questions[0].answers0[1]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[0].answers0[2] + '<br>'+'<br>');
+
+        $('#questions').append(questions[1].q1 + '<br>');
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[1].answers1[0]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[1].answers1[1]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[1].answers1[2] + '<br>'+'<br>');
+
+        $('#questions').append(questions[2].q2 + '<br>');
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[2].answers2[0]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[2].answers2[1]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[2].answers2[2] + '<br>'+'<br>');
+
+        $('#questions').append(questions[3].q3 + '<br>');
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[3].answers3[0]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[3].answers3[1]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[3].answers3[2] + '<br>'+'<br>');
+
+        $('#questions').append(questions[4].q4 + '<br>');
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[4].answers4[0]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[4].answers4[1]+"  ");
+        $('#questions').append('<input type="radio" name="radio_name" />' + questions[4].answers4[2] + '<br>'+'<br>');
+
+        if (document.getElementById("#q00").checked) {
+            score++;
+            console.log(score);
+        };
+
+    //     }
+    // };
 
 $("#start").on("click", function () {
     timer();
-    loadTrivia();
+    // loadTrivia();
 
     
 
@@ -77,4 +106,3 @@ $("#start").on("click", function () {
 //     answer1.push($('<div>', {i} ));
 // };
 // $("#choices").append();
-
